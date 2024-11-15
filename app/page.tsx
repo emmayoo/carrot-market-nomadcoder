354b1484af314ef2f2c90ae895f0ff29b354ede6
emmayoo <emmayoo9663@gmail.com>
2024-11-15 13:08:16 +0900

    TRY: Tailwind(*, children) & CSS (has)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div
+        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2
+        *:outline-none ring-transparent has-[:invalid]:ring has-[:invalid]:ring-red-300 transition-shadow"
+      >
         <input
-          className="flex-grow rounded-full h-10 bg-gray-200 pl-5 outline-none transition-shadow 
+          className="flex-grow rounded-full h-10 bg-gray-200 pl-5 transition-shadow 
           placeholder:drop-shadow 
           ring ring-transparent 
           focus:ring-green-500 focus:ring-offset-2 
@@ -19,7 +22,7 @@ export default function Home() {
           Email is required.
         </span>
         <button
-          className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none 
+          className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium
           md:px-10 
           bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400"
         >
