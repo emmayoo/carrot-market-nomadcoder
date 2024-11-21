be902fecabcee7f0c44943dc064333254afda3d3
emmayoo <emmayoo9663@gmail.com>
2024-11-21 13:17:59 +0900

    FEATURE: Log In by Email

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,5 +1,11 @@
 "use server";
+
+import bcrypt from "bcrypt";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+
 import { z } from "zod";
+import { redirect } from "next/navigation";
 
 import {
   PASSWORD_MIN_LENGTH,
@@ -7,8 +13,20 @@ import {
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
 
+const checkEmailExist = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  return !!user;
+};
+
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExist, "An account with this email doen not exist."),
   password: z
     .string({
       required_error: "Password is required",
@@ -23,11 +41,33 @@ export async function logIn(prevState: any, formData: FormData) {
     email: formData.get("email"),
     password: formData.get("password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data);
+
   if (!result.success) {
-    console.log(result.error.flatten());
     return result.error.flatten();
+  }
+
+  // if the user is found, check password hash
+  const user = await db.user.findUnique({
+    where: { email: result.data.email },
+    select: { id: true, password: true },
+  });
+
+  // SNS 로그인의 경우 password가 없을 수 있음
+  const ok = await bcrypt.compare(result.data.password, user?.password ?? "");
+
+  // log the user in
+  if (ok) {
+    const session = await getSession();
+    session.id = user!.id;
+
+    redirect("/profile");
   } else {
-    console.log(result.data);
+    return {
+      fieldErrors: {
+        email: [],
+        password: ["Wrong password."],
+      },
+    };
   }
 }
