17aca7acdb70b78f14b2f8ae7778f7288913f08e
emmayoo <emmayoo9663@gmail.com>
2024-11-26 14:43:57 +0900

    MOD: profile Suspense 사용

--- app/(tabs)/profile/page.tsx
+++ app/(tabs)/profile/page.tsx
@@ -1,10 +1,32 @@
-import { redirect } from "next/navigation";
+import db from "@/lib/db";
+
+import { notFound, redirect } from "next/navigation";
 import { getSession } from "@/lib/session";
-import { getUser } from "./action";
+import { Suspense } from "react";
+
+export async function getUser() {
+  const session = await getSession();
+  const id = session?.id;
+  if (!id) notFound();
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (user) {
+    return user;
+  }
+
+  notFound();
+}
+
+async function Username() {
+  const user = await getUser();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return <h1>welcome, {user?.username}!</h1>;
+}
 
 export default async function Profile() {
-  const user = await getUser();
-
   const logOut = async () => {
     "use server";
     const session = await getSession();
@@ -14,7 +36,9 @@ export default async function Profile() {
 
   return (
     <div>
-      <h1>welcome, {user?.username}!</h1>
+      <Suspense fallback={"Hello"}>
+        <Username />
+      </Suspense>
       <form action={logOut}>
         <button>Logout</button>
       </form>
